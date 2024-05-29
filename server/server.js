b17576cb7fb8ea50091e5cd4ffa857b01adf0841
Dilsher10 <Dilsher.dahri10@gmail.com>
2024-05-29 22:46:06 +0500

    changes

--- server/server.js
+++ server/server.js
@@ -5,27 +5,16 @@ const userRouter = require('./router/user-router');
 const adminRouter = require('./router/admin-router');
 const postRouter = require('./router/post-router');
 const cors = require('cors');
-const path = require('path');
+
 const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT;
 app.use(express.json());
 
-
-app.use(express.static(path.join(__dirname, '../client/out')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/out', 'index.html'));
-});
-
-
-
 app.use('/api/auth', userRouter);
 app.use('/api/auth', adminRouter);
 app.use('/api/auth', postRouter);
-
-
 
 connectDb().then(() => {
     app.listen(PORT, () => {
