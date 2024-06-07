1cc7775a991bd8fe551632f46d85fd2426e41a5a
Dilsher10 <Dilsher.dahri10@gmail.com>
2024-06-08 01:39:48 +0500

    changes

---
+++ client/components/admin/Navbar.jsx
@@ -0,0 +1,45 @@
+"use client"
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const Navbar = () => {
+
+    const [token, setToken] = useState('');
+
+    useEffect(() => {
+        const storedToken = typeof window !== 'undefined' ? localStorage.getItem("token") : null;
+        if (storedToken) {
+          setToken(storedToken);
+        }
+      }, []);
+
+    const handleLogout = () => {
+        setToken("");
+        localStorage.removeItem("token");
+        alert("Logout Successfully");
+        window.location.href = '/admin/login';
+    };
+
+    return (
+        <>
+            <nav className="navbar navbar-expand navbar-light sticky-top px-4 py-0" id="adminNavbar">
+                <div className="navbar-nav align-items-center ms-auto">
+                    <div className="nav-item dropdown">
+                        <Link href="#" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">
+                            <Image className="rounded-circle me-lg-2" src="/admin/user.jpg" alt="" width={40} height={40}/>
+                            <span className="d-none d-lg-inline-flex">John Doe</span>
+                        </Link>
+                        <div className="dropdown-menu dropdown-menu-end bg-light border-0 rounded-0 rounded-bottom m-0">
+                            <Link href="#" className="dropdown-item">My Profile</Link>
+                            <button className="dropdown-item" onClick={handleLogout}>Logout</button>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar;
