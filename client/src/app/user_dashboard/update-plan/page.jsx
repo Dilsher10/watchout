7f43fbb212608bd153e9f7e8bb7e9fe38edcbfe8
Dilsher10 <Dilsher.dahri10@gmail.com>
2024-07-06 04:42:42 +0500

    changes

--- client/src/app/user_dashboard/update-plan/page.jsx
+++ client/src/app/user_dashboard/update-plan/page.jsx
@@ -1,11 +1,20 @@
 "use client";
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Sidebar from '../../../../components/user_dashboard/Sidebar';
 import Navbar from '../../../../components/user_dashboard/Navbar';
 import Footer from '../../../../components/user_dashboard/Footer';
+import axios from 'axios';
 
 
 const Page = () => {
+    const [planData, setPlanData] = useState([]);
+    const readAll = async () => {
+        const res = await axios.get(process.env.NEXT_PUBLIC_API_URL + "/api/auth/user/view-plans");
+        setPlanData(res.data);
+    }
+    useEffect(() => {
+        readAll();
+    }, []);
     return (
         <>
             <div className="container-fluid position-relative bg-white d-flex p-0">
@@ -18,24 +27,18 @@ const Page = () => {
                                 <div className="bg-light rounded h-100 p-5">
                                     <section class="pricing-section">
                                         <div class="pricing-cards">
-                                            <div class="pricing-card">
-                                                <h2>Standard</h2>
-                                                <div class="price">
-                                                    <span class="currency">$</span>
-                                                    <span class="amount">75</span>
-                                                </div>
-                                                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia.</p>
-                                                <a href="#" class="btn">Buy Now</a>
-                                            </div>
-                                            <div class="pricing-card">
-                                                <h2>Premium</h2>
-                                                <div class="price">
-                                                    <span class="currency">$</span>
-                                                    <span class="amount">99</span>
-                                                </div>
-                                                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia.</p>
-                                                <a href="#" class="btn">Buy Now</a>
-                                            </div>
+                                            {
+                                                planData.map((item) => (
+                                                    <div class="pricing-card">
+                                                        <h2>{item?.plan}</h2>
+                                                        <div class="price">
+                                                            <span class="amount">{item?.price}</span>
+                                                        </div>
+                                                        <p>{item?.details}</p>
+                                                        <a href={`/user_dashboard/update-plan/${item._id}`} class="btn">Buy Now</a>
+                                                    </div>
+                                                ))
+                                            }
                                         </div>
                                     </section>
                                 </div>
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const connectDb = require('./utils/db');
 const userRouter = require('./router/user-router');
 const adminRouter = require('./router/admin-router');
 const postRouter = require('./router/post-router');
+const planRouter = require('./router/plan-router');
 const cors = require('cors');
 
 const app = express();
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use('/api/auth', userRouter);
 app.use('/api/auth', adminRouter);
 app.use('/api/auth', postRouter);
+app.use('/api/auth', planRouter);
 
 connectDb().then(() => {
     app.listen(PORT, () => {
